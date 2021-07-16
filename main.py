8a3864c7afc565258bec96f9ad721900f8f0a9c7
MadhanDevlpr <81558579+MadhanDevlpr@users.noreply.github.com>
2021-07-16 17:26:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkinter.ttk import *
+
+
+from time import strftime
+
+root = Tk()
+root.title('Clock by Madhan')
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000,time)
+
+label = Label(root, font=('ds_digital',80), background = 'black', foreground='white')
+label.pack(anchor='center')
+time()
+
+mainloop()
